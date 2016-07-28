8f69ffefc6083c4cc492cb6c5f64165c47f52ed9
Ronai Mate <ronaimate@gmail.com>
2016-07-28 14:54:02 +0200

    Login handler extends

--- app/src/main/java/com/andraskesik/codecoolapp/callbacks/LoginHandler.java
+++ app/src/main/java/com/andraskesik/codecoolapp/callbacks/LoginHandler.java
@@ -1,28 +1,10 @@
 package com.andraskesik.codecoolapp.callbacks;
 
 import android.content.Context;
-import android.content.pm.LauncherApps;
 
-import com.andraskesik.codecoolapp.model.Question;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class LoginHandler<User> implements Callback<User> {
-    Context mContext;
+public class LoginHandler<User> extends Handlers {
 
     public LoginHandler(Context context) {
-        mContext = context;
-    }
-
-    @Override
-    public void onResponse(Call<User> call, Response<User> response) {
-
-    }
-
-    @Override
-    public void onFailure(Call<User> call, Throwable t) {
-
+        super(context);
     }
 }
